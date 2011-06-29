590d82ebc105be34d5f727a548af2d1e985aab1f
Scott Gonyea <me@sgonyea.com>
2011-06-29 16:57:34 -0700

    Initial break up of haproxyctl. Added bin directory. Broke helper methods / variables into Mix-ins. Reduced the use of awk because Ruby does all that.

---
+++ lib/haproxyctl.rb
@@ -0,0 +1,89 @@
+require 'haproxyctl/environment'
+
+module HAProxyCTL
+  include Environment
+
+  def start
+    puts "starting haproxy..."
+    system("#{@exec} -f #{@config} -D -p #{@pid}")
+    newpidof = `pidof haproxy`.chomp
+    if ( newpidof =~ /\d+/ )
+      puts "haproxy is running on pid #{newpidof}"
+      return true
+    else
+      puts "error.  haproxy did not start!"
+      return nil
+    end
+  end
+
+  def stop(pid)
+    if pid
+      puts "stopping haproxy on pid #{pid}..."
+      system("kill #{pid}") || system("killall haproxy")
+      puts "... stopped"
+    else
+      puts "haproxy is not running!"
+    end
+  end
+
+  def reload(pid)
+    if ( pid )
+      puts "gracefully stopping connections on pid #{pid}..."
+      system("#{@exec} -f #{@config} -sf #{pid}")
+      puts "checking if connections still alive on #{pid}..."
+      nowpid = check_running()
+      while ( pid == nowpid )
+        puts "still haven't killed old pid.
+                            waiting 2s for existing connections to die...
+                            (ctrl+c to stop this check)"
+        sleep 2
+        nowpid = check_running() || 0
+      end
+      puts "reloaded haproxy on pid #{nowpid}"
+    else
+      puts "haproxy is not running!"
+    end
+  end
+
+  def unixsock(command)
+    require 'socket'
+
+    output=[]
+
+    ctl=UNIXSocket.open(@socket)
+    ctl.puts "#{command}"
+    while (line = ctl.gets) do
+      unless ( line =~ /Unknown command/ )
+        output << line
+      end
+    end
+    ctl.close
+
+    return output
+  end
+
+  def display_usage!
+    puts usage
+    exit
+  end
+
+  def usage
+    <<-USAGE
+usage: #{$0} <argument>
+  where argument can be:
+    start                     : start haproxy unless it is already running
+    stop                      : stop an existing haproxy
+    restart                   : immediately shutdown and restart
+    reload                    : gracefully terminate existing connections, reload #{@config_path}
+    status                    : is haproxy running?  on what ports per lsof?
+    configcheck               : check #{@config_path}
+    nagios                    : nagios-friendly status for running process and listener
+    cloudkick                 : cloudkick.com-friendly status and metric for connected users
+    show health               : show status of all frontends and backend servers
+    enable all server         : re-enable a server previously in maint mode on multiple backends
+    disable all server        : disable a server from every backend it exists
+    enable all EXCEPT server  : like 'enable all', but re-enables every backend except for <server>
+    disable all EXCEPT server : like 'disable all', but disables every backend except for <server>"
+USAGE
+  end
+end
---
+++ lib/haproxyctl/environment.rb
@@ -0,0 +1,56 @@
+module HAProxyCTL
+  module Environment
+    attr_accessor :pidof, :config_path, :config, :exec
+
+    def config_path
+      @config_path ||= ENV['HAPROXY_CONFIG'] || '/etc/haproxy/haproxy.cfg'
+    end
+
+    def config
+      @config ||= File.read(@config_path)
+    end
+
+    def has_exec?
+      !exec.nil?
+    end
+
+    def exec
+      return(@exec) if @exec
+
+      @exec   = ENV['HAPROXY_BIN']
+      @exec ||= `which haproxy`
+
+      if @exec.empty?
+        begin
+          `haproxy -v`
+          @exec = 'haproxy'
+        rescue Errno::ENOENT => e
+          @exec = nil
+        end
+      end
+
+      return(@exec)
+    end
+
+    def socket
+      @socket ||= begin
+        config.match /stats socket ([^\s]*)/
+        $1 || raise("Expecting 'stats socket <UNIX_socket_path>' in #{config_path}")
+      end
+    end
+
+    def pid
+      config.match /pidfile ([^\s]*)/
+      @pid = $1 || '/var/run/haproxy.pid'
+    end
+
+    # @return [String, nil] Returns the PID of HAProxy as a string, if running. Nil otherwise.
+    def check_running
+      pidof = `pidof haproxy`
+      pidof.strip!
+
+      return pidof if pidof =~ /^\d+$/
+    end
+    alias :pidof :check_running
+  end
+end
