789fd9c70a2f822ea13c35a3d467746b1e3e33d0
Scott Gonyea <me@sgonyea.com>
2011-06-30 20:44:25 -0700

    Added the Environment module + tests (migrating from my haproxyctl contrib).

---
+++ lib/rhapr/environment.rb
@@ -0,0 +1,87 @@
+module Rhapr
+  module Environment
+    attr_accessor :haproxy_pid, :config_path, :config, :exec
+
+    # @return [String] The path to the HAProxy configuration file. Set the ENV variable $HAPROXY_CONFIG to override defaults
+    def config_path
+      return(@config_path) if @config_path
+
+      if ENV['HAPROXY_CONFIG']
+        @config_path = ENV['HAPROXY_CONFIG']
+      else
+        %w{/etc/haproxy/haproxy.cfg /etc/haproxy.cfg /usr/local/etc/haproxy.cfg}.each {|cfg|
+          next unless File.exists?(cfg)
+
+          @config_path = cfg
+        }
+      end
+
+      return(@config_path)
+    end
+
+    # @return [String] The raw contents of the HAProxy configuration file.
+    def config
+      @config ||= begin
+                    File.read(config_path)
+                  rescue Errno::ENOENT => e
+                    raise RuntimeError.new("Error openning file '#{config_path}'. Exception from File.read: #{e.exception}")
+                  end
+    end
+
+    # @return [true, false] Whether or not the HAProxy executable can be found.
+    # @see Rhapr::Environment#exec
+    def has_exec?
+      !exec.nil?
+    end
+
+    # @return [String, nil] The path to the HAProxy executable will be returned, if found. Set ENV variable $HAPROXY_BIN to override
+    def exec
+      return(@exec) if @exec
+
+      @exec   = ENV['HAPROXY_BIN']
+      @exec ||= `which haproxy`
+
+      if @exec.empty?
+        begin
+          `haproxy -v`
+          @exec = 'haproxy'
+        rescue Errno::ENOENT => e
+          @exec = nil
+        end
+      end
+
+      return(@exec)
+    end
+
+    # @return [String] The path to the HAProxy stats socket.
+    # @raise [RuntimeError] Raised if no stats socket has been specified, in the HAProxy configuration.
+    # @todo: Should there be an ENV var for this? Perhaps allow config-less runs of rhapr?
+    def socket
+      @socket ||= begin
+                    config.match /stats\s+socket\s+([^\s]*)/
+                    $1 || raise(RuntimeError.new "Expecting 'stats socket <UNIX_socket_path>' in #{config_path}")
+                  end
+    end
+
+    # @return [String] Returns the path to the pidfile, specified in the HAProxy configuration. Returns an assumption, if not found.
+    # @todo: Should there even be an assumption? Does HAProxy create a pid file, if not told to by the configuration?
+    # @todo: Should there be an ENV var for this? Perhaps allow config-less runs of rhapr?
+    def pid
+      @pid  ||= begin
+                  config.match /pidfile ([^\s]*)/
+                  @pid = $1 || '/var/run/haproxy.pid'
+                end
+    end
+
+    # @return [String, nil] Returns the PID of HAProxy as a string, if running. Nil otherwise.
+    # @todo: Look for something other than pidof, for searching the process list.
+    #   Could read from the pid file, but there's potential that it will go stale.
+    def check_running
+      pidof = `pidof haproxy`
+      pidof.strip!
+
+      return pidof unless pidof.empty?
+    end
+    alias :pidof :check_running
+  end
+end
---
+++ spec/rhapr/environment_spec.rb
@@ -0,0 +1,120 @@
+require 'spec_helper'
+
+describe Rhapr::Environment do
+  class EnvTest
+    include Rhapr::Environment
+  end
+
+  before(:each) do
+    @env_test = EnvTest.new
+  end
+
+  describe '#config_path' do
+    it 'should set to the ENV variable, if present' do
+      ENV['HAPROXY_CONFIG'] = '/some/path.cfg'
+
+      @env_test.config_path.should == '/some/path.cfg'
+
+      # Clean up.
+      ENV.delete 'HAPROXY_CONFIG'
+    end
+
+    it 'should go down a list of pre-defined file names' do
+      File.stub!(:exists?).and_return(false)
+      File.should_receive(:exists?).with('/etc/haproxy.cfg').and_return(true)
+
+      @env_test.config_path.should == '/etc/haproxy.cfg'
+    end
+
+    it 'should be nil if config files do not exist and $HAPROXY_CONFIG is not set' do
+      File.stub!(:exists?).and_return(false)
+      @env_test.config_path.should be_nil
+    end
+  end
+
+  describe '#config' do
+    before(:each) do
+      File.stub!(:exists?).and_return(false)
+      File.should_receive(:exists?).with('/etc/haproxy.cfg').and_return(true)
+    end
+
+    it 'should raise an exception if it cannot read from #config_path' do
+      File.should_receive(:read).and_raise(Errno::ENOENT)
+
+      lambda {
+        @env_test.config
+      }.should raise_error(RuntimeError)
+    end
+
+    it 'should read and return the contents of a file' do
+      File.should_receive(:read).and_return { "I can haz cfg ?\n" }
+
+      @env_test.config.should == "I can haz cfg ?\n"
+    end
+  end
+
+  describe '#exec' do
+    it 'should set to the ENV variable, if present' do
+      ENV['HAPROXY_BIN'] = '/usr/local/bin/haproxy'
+
+      @env_test.exec.should == '/usr/local/bin/haproxy'
+
+      # Clean up.
+      ENV.delete 'HAPROXY_BIN'
+    end
+
+    it 'should call out to the `which` command to find haproxy, if the ENV var is not set' do
+      @env_test.should_receive(:`).with('which haproxy').and_return('/opt/bin/haproxy')
+
+      @env_test.exec.should == '/opt/bin/haproxy'
+    end
+
+    it 'should call out to haproxy directly, if all else fails' do
+      @env_test.should_receive(:`).with('which haproxy').and_return('')
+      @env_test.should_receive(:`).with('haproxy -v').and_return("HA-Proxy version 1.4.15 2011/04/08\nCopyright 2000-2010 Willy Tarreau <w@1wt.eu>\n\n")
+
+      @env_test.exec.should == 'haproxy'
+    end
+
+    it 'should be nil if none of the above worked' do
+      @env_test.should_receive(:`).with('which haproxy').and_return('')
+      @env_test.should_receive(:`).with('haproxy -v').and_raise(Errno::ENOENT)
+
+      @env_test.exec.should be_nil
+    end
+  end
+
+  describe '#socket' do
+    it 'should parse out the io socket from the config file' do
+      @env_test.should_receive(:config).and_return { config_for(:basic_haproxy) }
+
+      @env_test.socket.should == '/tmp/haproxy'
+    end
+
+    it 'should raise an error if it cannot derive an io socket from the config file' do
+      @env_test.should_receive(:config).and_return { config_for(:crappy_haproxy) }
+
+      lambda {
+        @env_test.socket
+      }.should raise_error(RuntimeError)
+    end
+  end
+
+  describe '#pid' do
+    it 'should parse out the pidfile from the config file' do
+      @env_test.should_receive(:config).and_return { config_for(:basic_haproxy) }
+
+      @env_test.pid.should == '/some/other/run/haproxy.pid'
+    end
+
+    it 'should return a default path if it cannot derive an io socket from the config file' do
+      @env_test.should_receive(:config).and_return { config_for(:crappy_haproxy) }
+
+      @env_test.pid.should == '/var/run/haproxy.pid'
+    end
+  end
+
+  describe '#check_running, #pidof' do
+    pending 'TBD'
+  end
+end
