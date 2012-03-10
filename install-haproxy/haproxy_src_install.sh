f850bd08cce59d9e811559c3ef115e15389e98ac
C. Flores <flores@petalphile.com>
2012-03-10 15:18:21 -0800

    support haproxy 1.4.20

--- install-haproxy/haproxy_src_install.sh
+++ install-haproxy/haproxy_src_install.sh
@@ -9,9 +9,10 @@ fi
 
 if [ $OS ]; then
 	if [ $OS = 'redhat' ]; then
-		yum install -y pcre-devel make gcc git;
+		yum install -y pcre-devel make gcc libgcc git;
 	elif [ $OS = 'debian' ]; then
-		apt-get install -y libpcre3 libpcre3-dev build-essential git;
+		apt-get update;
+		apt-get install -y libpcre3 libpcre3-dev build-essential libgcc1 git;
 	fi
 else
 	echo -e "I only understand Debian/RedHat/CentOS and this box does not appear to be any.\nExiting.\n- love, $0.";
@@ -21,17 +22,17 @@ fi
 # grab last stable.  HAProxy's site versions nicely - these will still be here after the next update
 mkdir /usr/local/src || echo "Oops, /usr/local/src exists!"
 cd /usr/local/src || exit 2
-wget http://haproxy.1wt.eu/download/1.4/src/haproxy-1.4.19.tar.gz
+wget http://haproxy.1wt.eu/download/1.4/src/haproxy-1.4.20.tar.gz
 
 # check the checksum
-MD5CHECK=`md5sum /usr/local/src/haproxy-1.4.19.tar.gz |awk '{print $1}'`
-if [ "$MD5CHECK" != "41392d738460dbf99295fd928031c6a4" ] ; then
+MD5CHECK=`md5sum /usr/local/src/haproxy-1.4.20.tar.gz |awk '{print $1}'`
+if [ "$MD5CHECK" != "0cd3b91812ff31ae09ec4ace6355e29e" ] ; then
         echo -e "MD5s do not match!\nBailing.";
         exit 2;
 fi
 
-tar xvfz haproxy-1.4.19.tar.gz
-cd haproxy-1.4.19
+tar xvfz haproxy-1.4.20.tar.gz
+cd haproxy-1.4.20
 
 if uname -a | grep x86_64 ; then
 	make TARGET=linux26 CPU=x86_64 USE_PCRE=1
@@ -40,6 +41,11 @@ else
 fi
 
 make install
+
+if [ -e /usr/sbin/haproxy ]; then
+  rm -f /usr/sbin/haproxy
+fi
+  
 ln -s /usr/local/sbin/haproxy /usr/sbin/haproxy
 
 # grab carlo's haproxyctl script/init
