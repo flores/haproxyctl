8f5e1a19dea9dd6aac69c72ac4f8b3e5b6416487
C. Flores <flores@petalphile.com>
2014-08-01 11:12:21 -0700

    support 1.5 in case anyone is using this installer

--- install-haproxy/haproxy_src_install.sh
+++ install-haproxy/haproxy_src_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash
 #
 # This installs latest HAProxy from source along with HAProxyCTL
 #
@@ -6,69 +6,78 @@
 # quick and dirty (but sometimes handy) installer.
 #
 
-HAPROXYVER="1.5.0"
-MD5EXPECTED="e33bb97e644e98af948090f1ecebbda9"
+HAPROXYVER="1.5.3"
+MD5EXPECTED="e999a547d57445d5a5ab7eb6a06df9a1"
 STARTINGDIR=$PWD
 
 # make sure we have make, pcre and junk
-if [ -e /etc/redhat-release ]; then
-	OS=redhat;
-elif [ -e /etc/debian_version ]; then
-	OS=debian;
+if [[ -e /etc/redhat-release ]]; then
+  OS=redhat;
+elif [[ -e /etc/debian_version ]]; then
+  OS=debian;
 fi
 
-if [ $OS ]; then
-	if [ $OS = 'redhat' ]; then
-		yum install -y pcre-devel make gcc libgcc git;
-	elif [ $OS = 'debian' ]; then
-		apt-get update;
-		apt-get install -y libpcre3 libpcre3-dev build-essential libgcc1 git;
-	fi
+if [[ -n $OS ]]; then
+  if [[ ${OS} == 'redhat' ]]; then
+    yum install -y pcre-devel make gcc libgcc git;
+  elif [[ ${OS} == 'debian' ]]; then
+    apt-get update;
+    apt-get install -y libpcre3 libpcre3-dev build-essential libgcc1 git;
+  fi
 else
-	echo -e "I only understand Debian/RedHat/CentOS and this box does not appear to be any.\nExiting.\n- love, $0.";
-	exit 2;
+  echo -e "I only understand Debian/RedHat/CentOS and this box does not appear to be any.\nExiting.";
+  exit 2;
 fi
 
 # grab last stable.  HAProxy's site versions nicely - these will still be here after the next update
+echo "I will try to build in /usr/local/src"
 mkdir /usr/local/src || echo "Oops, /usr/local/src exists!"
 cd /usr/local/src || exit 2
-wget http://haproxy.1wt.eu/download/1.5/src/haproxy-$HAPROXYVER.tar.gz
+if [[ -e /usr/local/src/haproxy-${HAPROXYVER}.tar.gz ]]; then
+  echo "using the existing haproxy-$HAPROXYVER.tar.gz.  If you have problems maybe rm it and we will grab it again"
+else
+  echo "I am grabbing ${HAPROXYVER} and will expand it under /usr/local/src/haproxy-${HAPROXYVER}"
+  wget http://haproxy.1wt.eu/download/1.5/src/haproxy-$HAPROXYVER.tar.gz
+fi
 
-# get rid of an existing haproxy
-if [ -e /usr/local/haproxy ]; then
+
+# check the checksum
+echo "Verifying the md5"
+MD5CHECK=$(md5sum /usr/local/src/haproxy-${HAPROXYVER}.tar.gz |awk '{print $1}')
+if [[ ${MD5CHECK} != ${MD5EXPECTED} ]] ; then
+  echo -e "MD5s do not match!\nBailing.";
+  exit 2;
+fi
+
+tar xvfz haproxy-${HAPROXYVER}.tar.gz
+rm haproxy-${HAPROXYVER}.tar.gz
+
+cd haproxy-${HAPROXYVER}
+
+echo "Making it!"
+if uname -a | grep x86_64 ; then
+  make TARGET=linux26 CPU=x86_64 USE_PCRE=1 || exit 2
+else
+  make TARGET=linux26 CPU=686 USE_PCRE=1 || exit 2
+fi
+
+if [[ -e /usr/local/haproxy ]]; then
+  echo "Removing old haproxy from /usr/local/haproxy"
   rm -fr /usr/local/haproxy
 fi
 
-# check the checksum
-MD5CHECK=`md5sum /usr/local/src/haproxy-$HAPROXYVER.tar.gz |awk '{print $1}'`
-if [ ${MD5CHECK} != ${MD5EXPECTED} ] ; then
-        echo -e "MD5s do not match!\nBailing.";
-        exit 2;
-fi
-
-tar xvfz haproxy-$HAPROXYVER.tar.gz
-rm haproxy-$HAPROXYVER.tar.gz
-
-cd haproxy-$HAPROXYVER
-
-if uname -a | grep x86_64 ; then
-	make TARGET=linux26 CPU=x86_64 USE_PCRE=1
-else 
-	make TARGET=linux26 CPU=686 USE_PCRE=1
-fi
-
+echo "Make installing!"
 make install
 
-if [ -e /usr/sbin/haproxy ]; then
+if [[ -e /usr/sbin/haproxy ]]; then
   rm -f /usr/sbin/haproxy
 fi
-  
+
 ln -s /usr/local/sbin/haproxy /usr/sbin/haproxy
 
-# grab carlo's haproxyctl script/init
-cd /usr/local
-if [ -e /usr/local/haproxyctl ]; then
-  cd haproxyctl;
+# grab haproxyctl script/init
+if [[ -e /usr/local/haproxyctl ]]; then
+  cd /usr/local/haproxyctl;
   git pull;
 else
   git clone https://github.com/flores/haproxyctl.git
@@ -76,11 +85,13 @@ else
 fi
 
 # remove make and gcc
-if [ $OS = 'redhat' ]; then
-	chkconfig --add haproxyctl;
-	yum remove -y gcc make
-elif [ $OS = 'debian' ]; then
-	apt-get purge -y build-essential
+if [[ ${OS} == 'redhat' ]]; then
+  chkconfig --add haproxyctl;
+  yum remove -y gcc make
+elif [[ ${OS} == 'debian' ]]; then
+  apt-get purge -y build-essential
 fi
 
+# back to where we started
 cd $STARTINGDIR
+echo "I think it is all done!"
